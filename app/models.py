454b4a775c52ddf2d9a2c5dfb413b63a8e4face6
bashhack (Marc Laughton) <info@marclaughton.com>
2020-01-06 00:54:59 -0500

    Add search functionality + update translations

--- app/models.py
+++ app/models.py
@@ -9,6 +9,47 @@ from jwt import DecodeError
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login, avatars
+from app.search import query_index, add_to_index, remove_from_index
+
+
+class SearchableMixin(object):
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+        if total == 0:
+            return cls.query.filter_by(id=0), 0
+        when = []
+        for i in range(len(ids)):
+            when.append((ids[i], i))
+        return cls.query.filter(cls.id.in_(ids)).order_by(
+            db.case(when, value=cls.id)), total
+
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
+        }
+
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(cls.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(cls.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(cls.__tablename__, obj)
+        session._changes = None
+
+    @classmethod
+    def reindex(cls):
+        for obj in cls.query:
+            add_to_index(cls.__tablename__, obj)
+
 
 followers = db.Table(
     "followers",
@@ -93,7 +134,7 @@ class User(UserMixin, db.Model):
         return User.query.get(user_id)
 
 
-class Post(db.Model):
+class Post(SearchableMixin, db.Model):
     __searchable__ = ['body']
 
     id = db.Column(db.Integer, primary_key=True)
@@ -104,3 +145,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"<Post {self.body}>"
+
+
+db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
--- app/search.py
+++ app/search.py
@@ -19,7 +19,7 @@ def remove_from_index(index, model):
 def query_index(index, query, page, per_page):
     if not current_app.elasticsearch:
         return [], 0
-    search = current_app.elasticsearch.search(index=index, doc_type=index,
+    search = current_app.elasticsearch.search(index=index,
                                               body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
                                                     'from': (page - 1) * per_page, 'size': per_page})
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
