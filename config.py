524894fbd9feefa249b149e3823f006e300dcd20
bashhack (Marc Laughton) <info@marclaughton.com>
2020-01-12 17:17:04 -0500

    Update config vars

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 import os
 
-from dotenv import load_dotenv
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
 
+    POSTGRES_USER = os.environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    POSTGRES_SERVER = os.environ.get("POSTGRES_SERVER")
+    POSTGRES_DB = os.environ.get("POSTGRES_DB")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"
+
     SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("DATABASE_URL") or f"sqlite:///{os.path.join(basedir, 'app.db')}"
+        DATABASE_URL or f"sqlite:///{os.path.join(basedir, 'app.db')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -19,7 +22,11 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = [os.environ.get("AADMIN_EMAIL")]
+
+    SES_REGION_NAME = os.environ.get("SES_REGION_NAME")
+    SES_REGION_SOURCE = os.environ.get("SES_REGION_SOURCE")
+
+    ADMINS = [os.environ.get("ADMIN_EMAIL")]
 
     POSTS_PER_PAGE = 25
 
@@ -28,3 +35,8 @@ class Config(object):
     MS_TRANSLATOR_KEY = os.environ.get("MS_TRANSLATOR_KEY")
 
     ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL")
+
+    REDIS_URL = os.environ.get("REDIS_URL") or "redis://"
+
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
